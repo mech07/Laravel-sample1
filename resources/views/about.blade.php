b079f913e701e3afe9a3547957be1f44f9b2221d
mech67 <mechelle.niezas@infinit-o.com>
2024-10-24 12:10:41 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About Us
+    </x-slot:heading>
+    <h1>About us</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Us
+    </x-slot:heading>
+    <h1>Contact us</h1>
+</x-layout>
